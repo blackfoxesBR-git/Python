b8a8f149fd8d54f5ed393d3067c9f075bd2790dd
Luis Ferreira <luis.ferreira.t0130008@edu.atec.pt>
2025-10-03 15:28:08 +0100

    Add exer1

---
+++ Exercícios/exer1.py
@@ -0,0 +1,10 @@
+
+segundos = int(input("Insira os segundos para converter em horas e em minutos: "))
+
+
+minutos = int(segundos/60)
+
+horas = int(segundos/120)
+
+print(segundos, minutos, horas)
+
